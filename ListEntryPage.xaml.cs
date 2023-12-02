d57a0ee65a26c33b2f2574b4437dfa83d57b5046
mpop14 <popmaria1401@yahoo.com>
2023-12-02 02:36:17 +0200

    laborator 8

---
+++ ListEntryPage.xaml.cs
@@ -0,0 +1,33 @@
+using pop_maria_lab7.Models;
+
+namespace pop_maria_lab7;
+
+public partial class NewPage1 : ContentPage
+{
+	public NewPage1()
+	{
+		InitializeComponent();
+	}
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        listView.ItemsSource = await App.Database.GetShopListsAsync();
+    }
+    async void OnShopListAddedClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ListPage
+        {
+            BindingContext = new ShopList()
+        });
+    }
+    async void OnListViewItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+        if (e.SelectedItem != null)
+        {
+            await Navigation.PushAsync(new ListPage
+            {
+                BindingContext = e.SelectedItem as ShopList
+            });
+        }
+    }
+}
---
+++ ListPage.xaml.cs
@@ -0,0 +1,23 @@
+namespace pop_maria_lab7;
+using pop_maria_lab7.Models;
+public partial class ListPage : ContentPage
+{
+	public ListPage()
+	{
+		InitializeComponent();
+	}
+    async void OnSaveButtonClicked(object sender, EventArgs e)
+    {
+        var slist = (ShopList)BindingContext;
+        slist.Date = DateTime.UtcNow;
+        await App.Database.SaveShopListAsync(slist);
+        await Navigation.PopAsync();
+    }
+    async void OnDeleteButtonClicked(object sender, EventArgs e)
+    {
+        var slist = (ShopList)BindingContext;
+        await App.Database.DeleteShopListAsync(slist);
+        await Navigation.PopAsync();
+    }
+
+}
